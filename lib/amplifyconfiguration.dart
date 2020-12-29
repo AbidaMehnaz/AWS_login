62377e2967c25a9fa8e650672d20dccd502e1ba2
AbidaMehnaz <mehnazkaro@gmail.com>
2020-12-29 22:56:30 +0800

    AWS Mobile app lab_8

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:1fd7897a-de85-4f7e-8d4f-1877d38411ae",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_ZHMgfN5kh",
+                        "AppClientId": "3osdrqops0jls0000h54kdhakr",
+                        "AppClientSecret": "1hcun2m1tk7euvbctmmtehcqd8adgjag703ct6qalsjmal27fnlt",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
